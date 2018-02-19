2513c43e6891146a12beff03af6e1c7cf69ad8e0
Rafał Palej <mail.rafixwpt@gmail.com>
2018-02-19 17:59:32 +0100

    MySQLConnector

---
+++ Elemental Tower Defense/Assets/Code/DatabaseManager/MySQLDatabaseManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySQLDatabaseManager : MonoBehaviour {
+	private readonly string _secret = "ELEMENTALTDSECRETKEY";
+
+	void Start() {
+		PostScore("mleko2", 22);
+	}
+
+	public void PostScore(string name, int score)
+	{
+		var www = new WWW (DatabaseConnectionUtilities.CreateUrl(name, score, _secret));
+		if (www.error != null) {
+			print ("There was error: " + www.error);
+		}
+	}
+}
---
+++ Elemental Tower Defense/Assets/Code/DatabaseManager/Utils/DatabaseConnectionUtilities.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DatabaseConnectionUtilities {
+
+	private static readonly string _addToLeaderboardUrl = "http://rpalej.pl/ELETD/ELETD.php?";
+
+	public static string CreateUrl(string name, int score, string secret)
+	{
+		string requestHash = ComputeMD5 (name + score + secret);
+		return _addToLeaderboardUrl + "name=" + name + "&score=" + score + "&hash=" + requestHash;
+	}
+
+	public static string ComputeMD5(string value)
+	{
+		var bytes = new System.Text.UTF8Encoding ().GetBytes (value);
+		var hashBytes = new System.Security.Cryptography.MD5CryptoServiceProvider ().ComputeHash (bytes);
+		var hashString = string.Empty;
+		for (var i = 0; i < hashBytes.Length; i++) {
+			hashString += System.Convert.ToString (hashBytes [i], 16).PadLeft (2, '0');
+		}
+
+		return hashString.PadLeft (32, '0');
+	}
+}
